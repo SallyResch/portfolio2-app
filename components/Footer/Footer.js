b4e40b14ca5e0388b1bf8d4d546225a985ec2ec7
Sillysally <sallyresch27@gmail.com>
2023-09-26 09:24:48 +0200

    added border color to application sections and color to navbar and footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
 	return (
-		<div>Footer</div>
+		<div className={styles.center}>Footer</div>
 	)
 }
