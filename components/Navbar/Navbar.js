a19e883ad60abb3431dcab5b64def2aadddf14e5
Sillysally <sallyresch27@gmail.com>
2023-09-29 11:07:43 +0200

    added easteregg

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,8 +1,18 @@
 import styles from "./Navbar.module.css";
+import { GiChest, GiOpenChest, GiOpenTreasureChest } from 'react-icons/gi';
 
 
 export default function Navbar() {
+
 	return (
-		<div className={styles.center}>Sally Resch</div>
+		<div className={styles.main}>
+
+			<h2 className={styles.center}>Sally Resch | Fullstack Java developer</h2>
+			<div className={styles.easteregg}>
+				<GiChest size={30} />
+				<GiOpenChest size={30} />
+				<GiOpenTreasureChest size={30} />
+			</div>
+		</div>
 	)
 }
