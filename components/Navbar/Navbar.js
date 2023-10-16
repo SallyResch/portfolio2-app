c2953cff0a1e9807d1ac9ffd3e5a1e7f2b711d56
Sillysally <92034603+SallyResch@users.noreply.github.com>
2023-10-16 15:19:34 +0200

    removed the openChest and just took the treasurechest

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 "use client"
 import React, { useState } from 'react';
 import styles from "./Navbar.module.css";
-import { GiChest, GiOpenChest, GiOpenTreasureChest } from 'react-icons/gi';
+import { GiChest, GiOpenTreasureChest } from 'react-icons/gi';
 
 export default function Navbar() {
-  const [isChestOpen, setIsChestOpen] = useState();
+  const [isChestOpen, setIsChestOpen] = useState(false); // Initialize state with false
 
   const handleChestClick = () => {
     setIsChestOpen(!isChestOpen);
@@ -14,8 +14,9 @@ export default function Navbar() {
     <div className={styles.main}>
       <h2 className={styles.center}>Sally Resch | Fullstack Java developer</h2>
       <div className={styles.easteregg} onClick={handleChestClick}>
-        {isChestOpen ? <GiOpenChest size={30} /> : <GiChest size={30} />}
+        {isChestOpen ? <GiOpenTreasureChest size={30} /> : <GiChest size={30} />}
       </div>
     </div>
   );
 }
+
