4069a3e1be31d3a1b0b453c5e6b35b4984068d18
Sillysally <sallyresch27@gmail.com>
2023-11-22 15:50:16 +0100

    added projects to the right webPage

--- app/[locale]/skills/page.js
+++ app/[locale]/skills/page.js
@@ -15,40 +15,7 @@ export default function Skills() {
 				<div className={`${styles.graphs} ${styles.fifth}`} ><h3>Agile</h3></div>
 				<div className={`${styles.graphs} ${styles.sixth}`} ><h3>HTML & CSS</h3></div>
 			</div>
-			<div className={styles.grid}>
-				<Link
-					href="https://reactproject-2noafu1o5-annaaste.vercel.app/"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2>Ingrii webapplication</h2>
-					<p>A next.js web application that finds replacement ingredients for you</p>
-				</Link>
-				<Link
-					href="https://reactmastery2-4w1154sqf-sallyresch27-gmailcom.vercel.app/"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2>MovieSearch</h2>
-					<p>A react web application where you can search on a movietitle and press the search symbol</p>
-				</Link>
-				<Link
-					href="https://www.heyey.se"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2>Heyey</h2>
-					<p>An application built in Next.js during an internship @Elevateyourself AB 20weeks</p>
-				</Link>
-				<Link
-					href="https://www.elevateyourself.se/sv"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2>Elevateyourself</h2>
-					<p>An application built in Next.js during an internship @Elevateyourself AB 20weeks</p>
-				</Link>
-			</div>
+
 		</main>
 	)
 }
