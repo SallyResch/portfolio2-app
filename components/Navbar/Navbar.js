e45e98c7ce0f7e845785ff66cab12c834c66a73e
Sillysally <sallyresch27@gmail.com>
2023-11-27 12:25:57 +0100

    added menu in navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import { GiChest, GiOpenTreasureChest } from 'react-icons/gi';
 import { Link } from 'next-intl';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { fallDown as Menu } from "react-burger-menu";
 
 export default function Navbar() {
   const t = useTranslations('Home');
@@ -19,9 +20,47 @@ export default function Navbar() {
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
-
+  var style = {
+    bmBurgerButton: {
+      position: "relative",
+      width: "45px",
+      height: "50px",
+      right: "0em",
+      top: "0em",
+      zIndex: 0,
+    },
+    bmCross: {
+      background: "#f6a022",
+    },
+    bmMenuWrap: {
+      position: "fixed",
+      height: "100%",
+      top: "0em",
+    },
+    bmMenu: {
+      background: "#ffffff",
+      padding: "2.5em 1.5em 0",
+      fontSize: "1.15em",
+      border: "2px solid #F6A022",
+      borderRadius: "9px 0 0 9px",
+    },
+    bmMorphShape: {
+      fill: "#373a47",
+    },
+    bmItemList: {
+      color: "#b8b7ad",
+      padding: "0.8em",
+    },
+    bmItem: {
+      display: "inline-block",
+    },
+    bmOverlay: {
+      background: "rgba(0, 0, 0, 0)",
+    },
+  };
   return (
     <div className={styles.main}>
+      {isMenuOpen && <div className={styles.overlay}></div>}
       <h2 className={styles.center}><Link href="/">Sally Resch</Link></h2>
       <div className={styles.easteregg} onClick={handleChestClick}>
         {isChestOpen ? <GiOpenTreasureChest size={40} /> : <GiChest size={40} />}
@@ -36,28 +75,27 @@ export default function Navbar() {
           <Link href="/" locale="sv" className={styles.pTagSmall}> {t("swedish")}</Link>
         </div>
       </div>
-      <div>
-
-        <div className={`${styles.webPageLinks} ${isMenuOpen ? styles.showMenu : ''}`}>
-          <Image src="/hambMenu.png" width={40} height={40} onClick={toggleMenu} />
-        </div>
-        {isMenuOpen && (
-          <div className={`${styles.webPageLinks} `}>
-            <Link href="/about" className={styles.wpl} onClick={toggleMenu}>
-              {t("about")}
-            </Link>
-            <Link href="/skills" className={styles.wpl} onClick={toggleMenu}>
-              {t("skills")}
-            </Link>
-            <Link href="/projects" className={styles.wpl} onClick={toggleMenu}>
-              {t("projects")}
-            </Link>
-            <Link href="/contact" className={styles.wpl} onClick={toggleMenu}>
-              {t("contact")}
-            </Link>
-          </div>
-        )}
-      </div>
+      <Menu
+        isOpen={isMenuOpen}
+        right
+        width={"250px"}
+        styles={style}
+        onStateChange={(state) => setMenuOpen(state.isMenuOpen)}
+        customBurgerIcon={<Image src="/hambMenu.png" width={40} height={40} />}
+      >
+        <Link href="/about" className={styles.wpl} onClick={toggleMenu}>
+          {t("about")}
+        </Link>
+        <Link href="/skills" className={styles.wpl} onClick={toggleMenu}>
+          {t("skills")}
+        </Link>
+        <Link href="/projects" className={styles.wpl} onClick={toggleMenu}>
+          {t("projects")}
+        </Link>
+        <Link href="/contact" className={styles.wpl} onClick={toggleMenu}>
+          {t("contact")}
+        </Link>
+      </Menu>
     </div >
   );
 }
