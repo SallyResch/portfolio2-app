b180e28e7535cba2263942abf6f38b1277b75501
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-07-08 08:18:34 +0200

    removed link that had error

--- app/[locale]/projects/page.js
+++ app/[locale]/projects/page.js
@@ -17,14 +17,7 @@ export default function Projects() {
 						<h2>Ingrii webapplication</h2>
 						<p>A next.js web application that finds replacement ingredients for you</p>
 					</Link>
-					<Link
-						href="https://reactmastery2-4w1154sqf-sallyresch27-gmailcom.vercel.app/"
-						className={styles.card}
-						target="_blank"
-						rel="noopener noreferrer">
-						<h2>MovieSearch</h2>
-						<p>A react web application where you can search on a movietitle and press the search symbol</p>
-					</Link>
+					
 					<Link
 						href="https://www.heyey.se"
 						className={styles.card}
